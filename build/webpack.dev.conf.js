7645ed0c34fba566b1454cf7fe5295e059340456
黄东 <huangdong23@meicloud.comgit config --global user.name >
2023-09-06 14:37:22 +0800

    feat: 添加loader

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -18,31 +18,6 @@ module.exports = merge(COMMON, {
 		path: resolve(__dirname, "../dist"),
 		filename: "bundle.js"
 	},
-	/**
-	 * 为自己编写的 loader 取一个别名 
-	 * 路径是编写 loader 的绝对路径
-	 */
-	resolveLoader: {
-		alias: {
-			'tpl-loader': resolve(__dirname, './loaders/tpl/index.js')
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tpl$/,
-				use: [
-					{
-						loader: 'tpl-loader',
-						options: {
-							log: true
-						}
-					}
-				]
-			}
-		]
-
-	},
 	plugins: [
 		new HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
